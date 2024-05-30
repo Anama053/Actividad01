38d2f84f3af2085d2bda3afcd00c338a5cce5360
Faver Xavier Neira Molina <fxneiram@gmail.com>
2024-05-29 21:39:29 -0500

    base form

--- fronted/src/app/app.component.ts
+++ fronted/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'knn-size';
+
+  userDataForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userDataForm = this.fb.group({
+      weight: ['', [Validators.required, Validators.min(1)]],
+      age: ['', [Validators.required, Validators.min(0)]],
+      height: ['', [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.userDataForm.valid) {
+      console.log(this.userDataForm.value);
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 }
