35bcc2f672a20d6fa9a2fbf502c742355c005e9d
Faver Xavier Neira Molina <fxneiram@gmail.com>
2024-05-29 21:58:57 -0500

    install tensorflow

--- fronted/src/app/app.component.ts
+++ fronted/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,20 +7,24 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'knn-size';
-
   userDataForm: FormGroup;
+  model: tf.LayersModel;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private ml: tf.LayersModel) {
     this.userDataForm = this.fb.group({
       weight: ['', [Validators.required, Validators.min(1)]],
       age: ['', [Validators.required, Validators.min(0)]],
       height: ['', [Validators.required, Validators.min(1)]]
     });
+
+    this.model = this.ml;
   }
 
-  ngOnInit(): void {}
+  async ngOnInit(): Promise<void> {
+    this.model = await tf.loadLayersModel('path/to/your/model.json');
+  }
 
   onSubmit(): void {
     if (this.userDataForm.valid) {
@@ -27,5 +32,17 @@ export class AppComponent {
     } else {
       console.error('Form is invalid');
     }
+  }
+
+  async predict(weight: number, age: number, height: number): Promise<number> {
+    const normalizedWeight = weight / 100;
+    const normalizedAge = age / 100;
+    const normalizedHeight = height / 100;
+  
+    const inputData = tf.tensor2d([[normalizedWeight, normalizedAge, normalizedHeight]]);
+  
+    const prediction = this.model.predict(inputData) as tf.Tensor<tf.Rank>;
+  
+    return prediction.dataSync()[0];
   }
 }
